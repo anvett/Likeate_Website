a6dbc7a97bb2870953a946d06ce6f6142987be3f
Andres <andveti@gmail.com>
2024-11-25 17:45:14 -0500

    Componente contador updated

--- src/app/cursos/curso-ia/page.jsx
+++ src/app/cursos/curso-ia/page.jsx
@@ -5,8 +5,10 @@ import React from 'react'
 
 export default function CursoIA() {
   return (
-    <div>
-      <CountdownTimer targetTime={new Date('2024-11-15T23:59:59')} onExpire={() => console.log('Curso finalizado')} />
+    <div className='flex flex-col items-center justify-center bg-black'>
+      <h2 className='landing-subtitle pb-spacing-3 text-yellow-400 text-center'>LA OFERTA DE 50% TERMINA EN</h2>
+      <CountdownTimer targetTime={new Date('2024-12-31T23:59:59')} onExpire={() => console.log('Curso finalizado')} />
+      
     </div>
   )
 }
